88e91de3170007f747f9d24823c00b9d3adce9b7
Ahmad Timori <temoria34@gmail.com>
2026-08-26 16:52:13 +0000

    We built a code that asks the use what the distance in miles of their trip is, what their mpg is, and what their ppg is. We then take these numbers and use them to calculate the gallons needed and the total cost for the etnire trip. we print these numbers and the other three inputs we asked the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    // Output "Enter distance"
+    println!("What is the distance of your trip in miles?");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let distance: i32 = input.trim().parse().expect("Not a number");
+
+    input = String::new();
+
+    // Output "Enter distance"
+    println!("What is the miles per gallon of your car or tuck?");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    println!("input: {input}");
+    let mpg: f64 = input.trim().parse().expect("MPG - Not a number");
+
+    input = String::new();
+
+    // Output "Enter distance"
+    println!("What is the price per gallon of gas currently? (In dollars)");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let ppg: f64 = input.trim().parse().expect("Not a number");
+    // Type conversion for distance to integer
+    let gallons = distance as f64 / mpg;
+    let total_cost = gallons * ppg;
+    println!("Gallons needed: {}", gallons);
+  println!("Total Cost (In Dollars): {}", total_cost);
+  println!("Distance: {}", distance);
+  println!("Efficiency (mpg): {}", mpg);
+println!("Price Per Gallon: {}", ppg);
 }
+
+
